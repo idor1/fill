dd2d9677605e21c7e462b2131f68dad78a2e6f91
idor <igor.doroshenko@gmail.com>
2016-02-23 23:21:01 +0200

    3 commit

--- src/main/java/com/test/Main.java
+++ src/main/java/com/test/Main.java
@@ -1,8 +1,6 @@
 package com.test;
 
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -27,86 +25,61 @@ public class Main {
         int[] K = {4, 3, 2, 1, 3, 1, 0};
 
         Arrays.stream(A).forEach(i -> System.out.print(" " + i));
-        System.out.println(" (2) : " + solution(A));//2
+        System.out.println(" (2) : " + count(A));//2
 
-//        Arrays.stream(B).forEach(i -> System.out.print(" " + i));
-//        System.out.println(" (8) : " + solution(B));//8
-//
-//        Arrays.stream(C).forEach(i -> System.out.print(" " + i));
-//        System.out.println(" (1) : " + solution(C));//1
-//
-//        Arrays.stream(D).forEach(i -> System.out.print(" " + i));
-//        System.out.println(" (1) : " + solution(D));//1
-//
-//        Arrays.stream(E).forEach(i -> System.out.print(" " + i));
-//        System.out.println(" (6) : " + solution(E));//6
-//
-//        Arrays.stream(F).forEach(i -> System.out.print(" " + i));
-//        System.out.println(" (8) : " + solution(F));//3 + 4 + 1 = 8
-//
-//        Arrays.stream(G).forEach(i -> System.out.print(" " + i));
-//        System.out.println(" (10) : " + solution(G));//3 + 4 + 3 = 10
-//
-//        Arrays.stream(H).forEach(i -> System.out.print(" " + i));
-//        System.out.println(" (11) : " + solution(H));//11
-//
-//        Arrays.stream(J).forEach(i -> System.out.print(" " + i));
-//        System.out.println(" (0) : " + solution(J));//0
-//
-//        Arrays.stream(K).forEach(i -> System.out.print(" " + i));
-//        System.out.println(" (3) : " + solution(K));//3
+        Arrays.stream(B).forEach(i -> System.out.print(" " + i));
+        System.out.println(" (8) : " + count(B));//8
+
+        Arrays.stream(C).forEach(i -> System.out.print(" " + i));
+        System.out.println(" (1) : " + count(C));//1
+
+        Arrays.stream(D).forEach(i -> System.out.print(" " + i));
+        System.out.println(" (1) : " + count(D));//1
+
+        Arrays.stream(E).forEach(i -> System.out.print(" " + i));
+        System.out.println(" (6) : " + count(E));//6
+
+        Arrays.stream(F).forEach(i -> System.out.print(" " + i));
+        System.out.println(" (7) : " + count(F));//2 + 4 + 1 = 7
+
+        Arrays.stream(G).forEach(i -> System.out.print(" " + i));
+        System.out.println(" (10) : " + count(G));//3 + 4 + 3 = 10
+
+        Arrays.stream(H).forEach(i -> System.out.print(" " + i));
+        System.out.println(" (11) : " + count(H));//11
+
+        Arrays.stream(J).forEach(i -> System.out.print(" " + i));
+        System.out.println(" (0) : " + count(J));//0
+
+        Arrays.stream(K).forEach(i -> System.out.print(" " + i));
+        System.out.println(" (3) : " + count(K));//3
     }
 
 
-    public static int solution(int[] a) {
+    public static int count(int[] a) {
         int smallerBorder = a[0];
         int volume = 0;
         if (a[0] > a[a.length - 1]) {
             smallerBorder = a[a.length - 1];
             for (int i = a.length - 1; i >= 0; i--) {
-                if (a[i] < smallerBorder) {
+                if (a[i] <= smallerBorder) {
                     volume += smallerBorder - a[i];
                 } else {
-                    smallerBorder = a[i];
+                    volume += count(Arrays.copyOfRange(a, 0, i + 1));
+                    break;
                 }
             }
         } else {
             for (int i = 0; i < a.length; i++) {
-                if (a[i] < smallerBorder) {
+                if (a[i] <= smallerBorder) {
                     volume += smallerBorder - a[i];
                 } else {
-                    smallerBorder = a[i];
+                    volume += count(Arrays.copyOfRange(a, i, a.length));
+                    break;
                 }
             }
         }
 
         return volume;
     }
-
-    private static List<Segment> parseToSegments(int[] a) {
-        Segment segment = new Segment();
-        segment.setA(a);
-
-        List<Segment> segments = new LinkedList<>();
-        segments.add(segment);
-
-        return segments;
-    }
-
-    private static int fillSergment(Segment segment) {
-        int smallerBorder = findSmallerBorder(segment.getA());
-
-        int volume = 0;
-        for (int i : segment.getA()) {
-            if (i < smallerBorder) {
-                volume += smallerBorder - i;
-            }
-        }
-        return volume;
-    }
-
-    private static int findSmallerBorder(int[] a) {
-        return a[0] > a[a.length - 1] ? a[a.length - 1] : a[0];
-    }
-
 }
