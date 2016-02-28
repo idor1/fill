457bc23959b57e5522e442c665514a95aa42fa35
idor <igor.doroshenko@gmail.com>
2016-02-28 15:47:17 +0200

    Interface and service added

---
+++ src/main/java/com/rainy/hills/service/VolumeCalculator.java
@@ -0,0 +1,15 @@
+package com.rainy.hills.service;
+
+/**
+ * This interface contains methods used to calculate
+ * the volume of the water which is left in hills after rain
+ */
+public interface VolumeCalculator {
+    /**
+     * Calculates the volume of water for given hills array.
+     * If input is {@code null} or empty array, the {@code 0} will be returned.
+     * @param hills array of elevations of the hills
+     * @return calculated volume for given {@code hills}
+     */
+    int calculateVolume(int[] hills);
+}
---
+++ src/test/java/com/rainy/hills/service/VolumeCalculatorTest.java
@@ -0,0 +1,36 @@
+package com.rainy.hills.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class VolumeCalculatorTest {
+    private VolumeCalculator volumeCalculator = new VolumeCalculatorImpl();
+
+    @Test
+    public void testCalculateVolume() {
+        assertEquals(2, volumeCalculator.calculateVolume(new int[]{3, 2, 4, 1, 2}));
+
+        assertEquals(8, volumeCalculator.calculateVolume(new int[]{4, 1, 1, 0, 2, 3}));
+
+        assertEquals(1, volumeCalculator.calculateVolume(new int[]{3, 2, 4}));
+
+        assertEquals(1, volumeCalculator.calculateVolume(new int[]{3, 2, 2, 1, 2}));
+
+        assertEquals(6, volumeCalculator.calculateVolume(new int[]{4, 1, 3, 0, 2, 3}));
+
+        assertEquals(7, volumeCalculator.calculateVolume(new int[]{3, 1, 4, 0, 4, 1, 2}));
+
+        assertEquals(10, volumeCalculator.calculateVolume(new int[]{4, 1, 4, 0, 4, 1, 5}));
+
+        assertEquals(11, volumeCalculator.calculateVolume(new int[]{4, 1, 4, 0, 4, 1, 5, 0, 1}));
+
+        assertEquals(0, volumeCalculator.calculateVolume(new int[]{4, 3, 2, 2, 1, 1, 0}));
+
+        assertEquals(3, volumeCalculator.calculateVolume(new int[]{4, 3, 2, 1, 3, 1, 0}));
+
+        assertEquals(0, volumeCalculator.calculateVolume(null));
+
+        assertEquals(0, volumeCalculator.calculateVolume(new int[0]));
+    }
+}
