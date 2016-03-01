72064235541482f9a97942274894581a2d0d44c0
idor1 <igor.doroshenko@gmail.com>
2016-03-01 17:38:02 +0200

    Adding resteasy

--- src/main/java/com/rainy/hills/web/rest/VolumeController.java
+++ src/main/java/com/rainy/hills/web/rest/VolumeController.java
@@ -3,11 +3,23 @@ package com.rainy.hills.web.rest;
 import com.rainy.hills.web.dto.HillsDto;
 import com.rainy.hills.web.dto.VolumeDto;
 
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 /**
  * This controller performs communication between Web and volume calculation services.
  */
+@Path("/volume")
+@Produces(MediaType.APPLICATION_JSON)
+//@Consumes(MediaType.APPLICATION_JSON)
 public class VolumeController {
-    public VolumeDto calculateVolume(HillsDto hills) {
-        return new VolumeDto();
+    @GET
+    public String calculateVolume() {
+        return "Test ok";
     }
 }
--- src/test/java/com/rainy/hills/it/facade/VolumeControllerTest.java
+++ src/test/java/com/rainy/hills/it/facade/VolumeControllerTest.java
@@ -1,4 +1,32 @@
 package com.rainy.hills.it.facade;
 
+import com.rainy.hills.unit.service.VolumeCalculator;
+import com.rainy.hills.unit.service.VolumeCalculatorImpl;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.net.URL;
+
+@RunWith(Arquillian.class)
 public class VolumeControllerTest {
+
+    @Deployment(testable = false)
+    public static Archive<?> createDeployment() {
+        return ShrinkWrap.create(WebArchive.class, "hills.war")
+                .addClass(VolumeCalculator.class)
+                .addClass(VolumeCalculatorImpl.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
+    @Test
+    public void testControllerAvailability(@ArquillianResource URL baseURI) {
+        System.out.println(baseURI.getHost());
+    }
 }
