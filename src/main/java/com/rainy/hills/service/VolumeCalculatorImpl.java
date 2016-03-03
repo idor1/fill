f29487784852214aecea8b809a31ac884e6c9628
idor <igor.doroshenko@gmail.com>
2016-03-04 00:23:04 +0200

    removing commented code

--- src/main/java/com/rainy/hills/service/VolumeCalculatorImpl.java
+++ src/main/java/com/rainy/hills/service/VolumeCalculatorImpl.java
@@ -11,50 +11,6 @@ public class VolumeCalculatorImpl implements VolumeCalculator {
 
     private static final Logger logger = LogManager.getLogger(VolumeCalculatorImpl.class);
 
-    //    @Override
-//    public int calculateVolume(int[] hills) {
-//        logger.debug("Incoming array: " + Arrays.toString(hills));
-//
-//        if (hills == null || hills.length == 0) {
-//            return 0;
-//        }
-//
-//        int smallerBorder;
-//        int volume = 0;
-//        int j = hills.length;
-//        while (j > 0) {
-//
-//            if (hills[0] > hills[hills.length - 1]) {
-//
-//                smallerBorder = hills[hills.length - 1];
-//                for (int i = hills.length - 1; i >= 0; i--) {
-//                    if (hills[i] <= smallerBorder) {
-//                        volume += smallerBorder - hills[i];
-//                        j--;
-//                    } else {
-//                        hills = Arrays.copyOfRange(hills, 0, i + 1);
-//                        break;
-//                    }
-//                }
-//            } else {
-//                smallerBorder = hills[0];
-//                for (int i = 0; i < hills.length; i++) {
-//                    if (hills[i] <= smallerBorder) {
-//                        volume += smallerBorder - hills[i];
-//                        j--;
-//                    } else {
-//                        hills = Arrays.copyOfRange(hills, i, hills.length);
-//                        break;
-//                    }
-//                }
-//            }
-//        }
-//
-//        logger.debug("Calculated volume: " + volume);
-//
-//        return volume;
-//    }
-//
     @Override
     public int calculateVolume(List<Integer> hills) {
         logger.debug("Incoming array: " + hills);
@@ -66,10 +22,10 @@ public class VolumeCalculatorImpl implements VolumeCalculator {
         int smallerBorder;
         int volume = 0;
         int j = hills.size();
+
         while (j > 0) {
 
             if (hills.get(0) > hills.get(hills.size() - 1)) {
-
                 smallerBorder = hills.get(hills.size() - 1);
                 for (int i = hills.size() - 1; i >= 0; i--) {
                     if (hills.get(i) <= smallerBorder) {
@@ -92,6 +48,7 @@ public class VolumeCalculatorImpl implements VolumeCalculator {
                     }
                 }
             }
+
         }
 
         logger.debug("Calculated volume: " + volume);
